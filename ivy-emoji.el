f404d60b9a8ee29eef1e7671782022181c61c277
Gabriele Bozzola <sbozzolator@gmail.com>
2020-02-18 15:29:58 -0700

    Add two spaces after periods in commentary

--- ivy-emoji.el
+++ ivy-emoji.el
@@ -27,9 +27,9 @@
 ;; ivy-emoji provides a convenient way to insert any emoji in any buffer using
 ;; ivy to select the emoji by its name.
 
-;; A font that supports emoji is needed. The best results are obtained with
-;; Noto Color Emoji or Symbola. It might be necessary to instruct Emacs to use
-;; such font with a line like the following.
+;; A font that supports emoji is needed.  The best results are obtained with Noto
+;; Color Emoji or Symbola.  It might be necessary to instruct Emacs to use such
+;; font with a line like the following.
 ;;
 ;; (set-fontset-font t 'symbol
 ;;                      (font-spec :family "Noto Color Emoji") nil 'prepend)
